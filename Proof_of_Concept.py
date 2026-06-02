ca34087bee76439721f70d1c7694e858371bee3d
glodegloria <gloriuchi22@gmail.com>
2026-06-02 18:59:10 +0200

    Added the Proof of concept and some comments to principal

--- Proof_of_Concept.py
+++ Proof_of_Concept.py
@@ -7,6 +7,10 @@ import mat73
 import numpy as np
 
 
+########################################
+#If you want to plot the map from the proof_of_concept
+########################################
+
 #def plot_robinson(X, Y, Dinterp, title='Interpolated Data (Robinson)'):
 #    fig = plt.figure(figsize=(12, 6))
 #    ax = plt.axes(projection=ccrs.Robinson(central_longitude=0))
@@ -22,12 +26,13 @@ import numpy as np
 #    plt.savefig(f'alphadiv/{title}.png', dpi=300, bbox_inches='tight')
 #    plt.show()
 
+#It saves all the constants
 kfood = 0.5
-Kmin=19
 lonWindow = 2.5
 latWindow = 2.5
-spec_min_mean = 0.002
 ext_pattern = 4
+ext_intercept_shelf_mean=0
+ext_slope_mean=0
 
 #Kmax_mean = 161
 #Kmin = 19
@@ -35,63 +40,61 @@ ext_pattern = 4
 #spec_max_mean = 0.035
 #Q10_mean 1.75
 
-params_proposed = [161, 0.035, 2, 0, 0]
+#It saves all the parameters with the reference value
+params_proposed = [161, 19, 0.035, 0.002, 2]
 
 ####################################################################
 # Load the input data
 ####################################################################
-data_food_temp=scipy.io.loadmat('data_input/Point_foodtemp.mat')
+data_food_temp=scipy.io.loadmat('data/Point_foodtemp.mat')
 
-#print(data_food_temp.keys())
-#food_ocean=data['food_ocean']
 food_shelf=data_food_temp['food_shelf']
-#temp_ocean=data['temp_ocean']
 temp_shelf=data_food_temp['temp_shelf']
 
 
-data_LonDeg=scipy.io.loadmat('data_input/LonDeg.mat')
-#print(data_LonDeg.keys())
+data_LonDeg=scipy.io.loadmat('data/LonDeg.mat')
 
 LonDeg=data_LonDeg['LonDeg']
 
-data_point_ages=scipy.io.loadmat('data_input/Point_ages_xyz')#
-#print(data_point_ages.keys())
-#
+data_point_ages=scipy.io.loadmat('data/Point_ages_xyz')
 Point_timeslices=data_point_ages['Point_timeslices'].astype(int)
-#Point_timeslices = Point_timeslices[0]
 shelf_lonlatAge=data_point_ages['shelf_lonlatAge']
 
-data_Mask=mat73.loadmat('data_input/landShelfOceanMask.mat')
-#print(data_Mask.keys())
+data_Mask=mat73.loadmat('data/landShelfOceanMask.mat')
 
 landShelfOcean_Lat=data_Mask['landShelfOcean_Lat']
 landShelfOcean_Lon=data_Mask['landShelfOcean_Lon']
 landShelfOceanMask=data_Mask['landShelfOceanMask']
 landShelfOceanMask = np.flip(landShelfOceanMask, axis=2)
 
+#It calls the model, as it will serve as a reference, this is the proof model
 model="proof"
 
-[rho_shelf,K_shelf, ext_index]=rhonet_evo(kfood,Kmin,food_shelf,temp_shelf,ext_pattern,params_proposed[0],spec_min_mean,params_proposed[1], params_proposed[2],params_proposed[3],params_proposed[4],shelf_lonlatAge,Point_timeslices[0], model)
+#Calls the rhonet_evo function to calculate the rho_shelf (net diversification rate) and K_shelf (carrying capacity) matrices.
+[rho_shelf,K_shelf, ext_index]=rhonet_evo(kfood,params_proposed[1],food_shelf,temp_shelf,ext_pattern,params_proposed[0],params_proposed[3],params_proposed[2], params_proposed[4],params_proposed[3],params_proposed[4],shelf_lonlatAge,Point_timeslices[0], model)
 
+#Calls the alphadiv function to calculate the D (current diversity) and D_shelf(diversity through the years) matrix. 
 [rho_shelf_eff,D_shelf]=alphadiv(Point_timeslices,shelf_lonlatAge,rho_shelf,K_shelf,latWindow,lonWindow,LonDeg, ext_index)
 
-#[D,X,Y]=inditek_gridding_alphadiv(Point_timeslices[0],ext_pattern,rho_shelf,D_shelf,rho_shelf_eff,shelf_lonlatAge,landShelfOceanMask,landShelfOcean_Lat,landShelfOcean_Lon,params_proposed[0])
-
-#print("D shape:", D.shape)
+#If you want to plot the map, you call the function to plot it here
 #for i in range(D.shape[2]):
 #    Dinterp = D[:, :, i]
 #    title = f'Capa {i} - Tiempo {Point_timeslices[0][i]} Ma'  # Ajusta según tus datos
 #    plot_robinson(X, Y, Dinterp, title)
 
+#Calls the inditek_gridMean_alphadiv function to calculate the grid that covers the earth surface and the mean of the diversity in each grid cell.
+
 [X, Y, proof]=inditek_gridMean_alphadiv(D_shelf,shelf_lonlatAge,landShelfOcean_Lat,landShelfOcean_Lon, landShelfOceanMask)
 
+#Here the random Gaussian error is created
 mu=0
 sigma=2
 
 random_values = np.random.normal(loc=mu, scale=sigma, size=proof.shape)
 
+#This error is added to the proof's data.
 proof=proof+random_values
 
 proof = np.clip(proof,1,None)
 
-np.savez("probita.npz", proof=proof)
+np.savez("data/proof_of_concept.npz", proof=proof)
--- haversine_distance.py
+++ haversine_distance.py
@@ -2,18 +2,24 @@ from numpy import radians, cos, sin, sqrt, arctan2
 
 def haversine_distance(lonlat1, lonlat_array):
 
+    #Become grades into radians
     radius=6371 # Earth's radius in kilometers
     lat1=radians(lonlat1[1])
     lon1 = radians(lonlat1[0])
     lat2 = radians(lonlat_array[:,1])
-    lon2 = radians(lonlat_array[:,0])
+    lon2 = radians(lonlat_array[:, 0])
 
+    #Calculate the difference for latitudes and longitudes
     deltaLat = lat2-lat1
     deltaLon = lon2-lon1
 
+    #Calculate the Haversine distance
     a= sin(deltaLat/2)**2+cos(lat1)*cos(lat2)*sin(deltaLon/2)**2
+    #Correct to avoid round mistakes
     a[a>1]=1
+    #Central angle between points
     c= 2*arctan2(sqrt(a), sqrt(1-a))
+    #Become the angle into kilometres
     distances = radius * c
 
     return distances
